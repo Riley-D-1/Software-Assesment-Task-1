753edd8c8983d9153be638ea6623baa891483194
Riley-D-1 <riley.dowse@education.nsw.gov.au>
2025-03-19 10:08:44 +1100

    API and App updates
    Added improved error handling. Started to comment the code. Confirmed all API requests were working with test cases. The API didn't work as expected which led me to problem solve and change parts of the code. I achieved this by using http status codes and trial and error and ending up solving all of the problems. The immediate implications are they I can no begin to better incorporate the API's data when the time comes. Next I need to continue the developmental phase and continuing to test.

--- Nasa_API_module.py
+++ Nasa_API_module.py
@@ -1,7 +1,8 @@
 import requests
 import datetime
 import pandas
-API_KEY = "DEMO_KEY"
+import time
+API_KEY = "b9Df79dEOc4JG9m3nfKxBpK9REKk8uuAENIxKcKc"
 # Here we have the fetching of the APOD data
 def APOD():
     params={"api_key": API_KEY,}
@@ -10,40 +11,91 @@ def APOD():
     if response.status_code == 200:
         data = response.json()
         return data
+    elif response.status_code == 429:
+        print(f"The API key can make{response.headers["X-RateLimit-Remaining"]} more requess")
+        print(f"The server has been requested too many times. The program will resume in {response.headers["Retry-After"]} seconds")
+        time.sleep(int(response.headers["Retry-After"]))
+        try:
+            response = requests.get(APOD_URL, params=params)
+            data = response.json()
+            return data
+        except:
+            print("Failed to fetch APOD. The server responded with the status code of "+str(response.status_code)) 
+            return None
     else:
-        print("Failed to fetch APOD.") 
+        print("Failed to fetch APOD. The server responded with the status code of "+str(response.status_code)) 
         return None
-# Here we have the fetching of the APOD data
+# Here we have the fetching of the NeoWs Feed data
 def NeoWs_Feed(startdate,enddate):
-    APOD_URL = "https://api.nasa.gov/neo/rest/v1/feed" 
+    NeoWs_Feed_URL = "https://api.nasa.gov/neo/rest/v1/feed" 
     params={"api_key": API_KEY, "start_date":startdate,"end_date":enddate}
-    response = requests.get(APOD_URL, params=params)
+    response = requests.get(NeoWs_Feed, params=params)
     if response.status_code == 200:
         data = response.json()
         return data
+    elif response.status_code == 429:
+        print(f"The API key can make{response.headers["X-RateLimit-Remaining"]} more requess")
+        print(f"The server has been requested too many times. The program will resume in {response.headers["Retry-After"]} seconds")
+        time.sleep(int(response.headers["Retry-After"]))
+        try:
+            response = requests.get(NeoWs_Feed_URL, params=params)
+            data = response.json()
+            return data
+        except:
+            print("Failed to fetch NeoWs Feed. The server responded with the status code of "+str(response.status_code))  
+            return None
     else:
-        print("Failed to fetch NeoWs Feed.") 
+        print("Failed to fetch NeoWs Feed. The server responded with the status code of "+str(response.status_code))  
         return None
-# Here we have the fetching of the APOD data    
+# Here we have the fetching of the NeoWs lookup data    
 def NeoWs_lookup(astroid_id):
-    NeoWs_lookup_URL = "https://api.nasa.gov/neo/rest/v1/neo/" 
-    params={"api_key": API_KEY, "astroid_id":astroid_id}
-    response = requests.get(NeoWs_lookup_URL, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        print("Failed to fetch NeoWs lookup.") 
-        return None
-
-def NeoWs_overview():
-    NeoWs_lookup_URL = "https://api.nasa.gov/neo/rest/v1/neo/browse/" 
+    NeoWs_lookup_URL = f"https://api.nasa.gov/neo/rest/v1/neo/{astroid_id}" 
     params={"api_key": API_KEY}
     response = requests.get(NeoWs_lookup_URL, params=params)
     if response.status_code == 200:
         data = response.json()
         return data
+    elif response.status_code == 429:
+        print(f"The API key can make{response.headers["X-RateLimit-Remaining"]} more requess")
+        print(f"The server has been requested too many times. The program will resume in {response.headers["Retry-After"]} seconds")
+        time.sleep(int(response.headers["Retry-After"]))
+        try:
+            response = requests.get(NeoWs_lookup_URL, params=params)
+            data = response.json()
+            return data
+        except:
+            print("Failed to fetch NeoWs lookup. The server responded with the status code of "+str(response.status_code)) 
+            return None
+
     else:
-        print("Failed to fetch NeoWs Overview.") 
+        print("Failed to fetch NeoWs lookup. The server responded with the status code of "+str(response.status_code)) 
         return None
 
+# Here we have the fetching of the NeoWs overview data.
+
+def NeoWs_overview():
+    NeoWs_overview_URL = "https://api.nasa.gov/neo/rest/v1/neo/browse" 
+    params={"api_key": API_KEY}
+    response = requests.get(NeoWs_overview_URL, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 429:
+        print(f"The API key can make{response.headers["X-RateLimit-Remaining"]} more requess")
+        print(f"The server has been requested too many times. The program will retry/resume in {response.headers["Retry-After"]} seconds")
+        time.sleep(int(response.headers["Retry-After"]))
+        try:
+            response = requests.get(NeoWs_overview_URL, params=params)
+            data = response.json()
+            return data
+        except:
+            print("Failed to fetch NeoWs Overview. The server responded with the status code of "+str(response.status_code))  
+            return None
+    else:
+        print("Failed to fetch NeoWs Overview. The server responded with the status code of "+str(response.status_code))  
+        return None
+
+#print(NeoWs_Feed("2025-03-18","2025-03-19"))
+#print(NeoWs_overview())
+#print(NeoWs_lookup(3542519))
+print(APOD())
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import *
 app = Flask(__name__)
-
+# Homepage route for better+cleaner intergration
 @app.route('/')
 def home():
     return render_template('index.html')
-
+# App route to NeoWs for better+cleaner intergration
 @app.route('/apod')
 def APOD():
     return render_template('apod.html',url="test")
-
+# App route to NeoWs for better+cleaner intergration
 @app.route('/NeoWs')
 def home():
     return render_template('NeoWs.html')
